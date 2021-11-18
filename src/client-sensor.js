84c890a5a61bb97e180283dff1bca58913c31ff4
Cristiano Rodrigues <devcristiano.rodrigues@gmail.com>
2021-11-18 00:09:54 -0300

    feature: criando sensor

---
+++ src/client-sensor.js
@@ -0,0 +1,56 @@
+const net = require("net");
+//identificador único do sensor
+const sensorId = Date.now();
+
+const options = {
+  host: "localhost",
+  port: 9000,
+  sensorId: sensorId
+}
+
+let details = {sensorId: sensorId, temperatura: 30, umidade:1, nivelCO2: 3};
+
+const client = new net.Socket();
+
+let statusInterval = null;
+
+function disconnect(){
+  console.debug("Successfully disconnected from server!");
+  client.destroy();
+}
+
+function clearStatusInterval(){
+  statusInterval && clearInterval(statusInterval);
+}
+
+function setStatusInterval(socket) {
+  if (statusInterval) {
+    clearInterval(statusInterval);
+  }
+
+  statusInterval = setInterval(() => {
+    const buffer = Buffer.from(JSON.stringify(details));
+    client.write(buffer);
+  }, 1000);
+}
+
+client.on("data", function(chunk){
+  console.log(JSON.parse(chunk));
+
+  client.end();
+})
+
+// o sensor se conecta ao gerenciador
+client.connect(options, () => {
+  console.log(`Sensor '${options.sensorId}' connected to server successfully`);
+  //Sensor faz sua identificação para o servidor
+  client.write("HEAD / LCM/1.0\r\n");
+  client.write(`Host: ${options.host}\r\n`);
+  client.write(`User-Agent: Sensor Client\r\n`);
+  client.write(`SessionId: ${details.sensorId}`);
+
+  // inicializa o envio de mensagens para o servidor a cada 1 segundo
+  setStatusInterval(client);
+})
+
+client.on("close", ()=>{disconnect(); clearStatusInterval();})
--- src/server.js
+++ src/server.js
@@ -8,22 +8,39 @@ let details = {
   umidade: 0,
   oxigenacao: 0,
   batimentos: 0,
-}
+};
+
+let lastestSensorLogs = [];// array do tipo {sensorID: number, message: string, remoteAddress: string}
 
 function clearStatusInterval() {
   statusInterval && clearInterval(statusInterval);
 }
 
-function setStatusInterval(socket) {
-  if (statusInterval) {
-    clearInterval(statusInterval);
-  }
+function addSensor(socket, data){
+  const newSensor= {sensorId: data.sensorId, remoteAddress: socket.remoteAddress, message: JSON.stringify(data) };
+  //remove o sensor caso ele exista
+  lastestSensorLogs = lastestSensorLogs.filter(sensor => sensor.sensorId !== data.sensorId);
+  // adicina o sensor novamente, só que com a ultima mensagem enviada.
+  lastestSensorLogs.push(newSensor);
+}
 
-  statusInterval = setInterval(() => {
-    // TODO: Está passando como JSON stringify aqui, mas precisa ser
-    // um buffer. Deem uma olhada como passar aqui e receber no client
-    socket.write(JSON.stringify(details));
-  }, 1000);
+// verifica se é um JSON válido
+function isJSON(text) {
+  if (
+    /^[\],:{}\s]*$/.test(
+      text
+        .replace(/\\["\\\/bfnrtu]/g, '@')
+        .replace(
+          /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
+          ']'
+        )
+        .replace(/(?:^|:|,)(?:\s*\[)+/g, '')
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 server.on('connection', (socket) => {
@@ -31,42 +48,44 @@ server.on('connection', (socket) => {
   console.log('New client is connected %s', remoteAddress);
 
   socket.on('data', (data) => {
-    const parsedData = JSON.parse(data);
+    //caso a mensagem seja um JSON, ele irá realizar as devidas ações
+    if(isJSON(data.toString())){
+      const parsedData = JSON.parse(data.toString());
 
-    if (parsedData?.action === 'aquecer') {
-      details = {
-        ...details,
-        temperaturaDoAr: details.temperaturaDoAr + 1
-      };
-    } else if (parsedData?.action === 'umidificar') {
-      details = {
-        ...details,
-        umidade: details.umidade + 1
-      };
-    } else if (parsedData?.action === 'circularAr') {
-      details = {
-        ...details,
-        oxigenacao: details.oxigenacao + 1
-      };
-    } else {
-      console.log('Action not recognized..');
+      addSensor(socket, parsedData);
+
+      if (parsedData?.action === 'aquecer') {
+        details = {
+          ...details,
+          temperaturaDoAr: details.temperaturaDoAr + 1
+        };
+      } else if (parsedData?.action === 'umidificar') {
+        details = {
+          ...details,
+          umidade: details.umidade + 1
+        };
+      } else if (parsedData?.action === 'circularAr') {
+        details = {
+          ...details,
+          oxigenacao: details.oxigenacao + 1
+        };
+      } else {
+        //mostrando mensagens dos sensores
+        console.log(parsedData);
+      }
+    }else{
+      console.log(data.toString());
     }
-
-    socket.write(JSON.stringify(details));
-    setStatusInterval(socket);
   });
 
   socket.once('close', () => {
+    lastestSensorLogs = [];
     console.log('Connecting from %s closed', remoteAddress);
   });
 
   socket.on('error', (err) => {
     console.log('Connection %s error: %s', remoteAddress, err.message);
   });
-
-  if (!statusInterval) {
-    setStatusInterval(socket)
-  }
 });
 
 server.on('close', () => {
