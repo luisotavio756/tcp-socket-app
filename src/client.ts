7cfa6f2f386aad1fe2ddd70cc01b7bf12509c88d
Mateus Fonseca <mateus.xfl@gmail.com>
2021-11-30 09:59:26 -0300

    Comments on client code

--- src/client.ts
+++ src/client.ts
@@ -3,15 +3,18 @@ import net from 'net';
 import readlineSync from 'readline-sync';
 import { createISOMessage } from './utils';
 
+// Parâmetros do menu, no caso: em quanto tempo ele vai aparecer novamente.
 interface IMenuParams {
   milliseconds: number;
 }
 
+// Opções de conexão do socket.
 const options: net.SocketConnectOpts = {
   host: 'localhost',
   port: 9000,
 };
 
+// Crio o socket.
 const client = new net.Socket();
 
 function disconnect() {
@@ -22,28 +25,31 @@ function showMenu({
   milliseconds
 }: IMenuParams) {
   setTimeout(() => {
+    // Fico chamando action menu a cada milliseconds passados por parâmetro.
     actionsMenu();
   }, milliseconds)
 }
 
+// ( Só aparece uma vez ) Função que recebe os parâmetros máximos e mínimos de cada sensor.
 function menu_parametros() {
 
   let valueMinSensorTemperature: string | number = readlineSync
     .question('\n\nHow is the min value from the temperature sensor?\n');
-    valueMinSensorTemperature = parseInt(valueMinSensorTemperature, 10);
+  valueMinSensorTemperature = parseInt(valueMinSensorTemperature, 10);
 
   let valueMaxSensorTemperature: string | number = readlineSync
     .question('\n\nHow is the max value from the temperature sensor?\n');
-    valueMaxSensorTemperature = parseInt(valueMaxSensorTemperature, 10);
+  valueMaxSensorTemperature = parseInt(valueMaxSensorTemperature, 10);
 
   let valueMaxSensorHumidity: string | number = readlineSync
     .question('\n\nHow is the max value from the humidity sensor?\n');
-    valueMaxSensorHumidity = parseInt(valueMaxSensorHumidity, 10);
+  valueMaxSensorHumidity = parseInt(valueMaxSensorHumidity, 10);
 
   let valueMaxSensorCo2: string | number = readlineSync
     .question('\n\nHow is the max value from the CO2 sensor?\n');
-    valueMaxSensorCo2 = parseInt(valueMaxSensorCo2, 10);
+  valueMaxSensorCo2 = parseInt(valueMaxSensorCo2, 10);
 
+  // Mando uma action do client para o server, e o server irá tratar da atribuição de valores máximos e mínimos para os sensores.
   const payload = createISOMessage({
     emitter: 'Client',
     message: {
@@ -61,6 +67,7 @@ function menu_parametros() {
 
 }
 
+// ( Aparece de acordo com o parâmetro de showMenu ) Responsável por mostrar as opções do LOG do sensores.
 function actionsMenu() {
 
   const readLine = readlineSync
@@ -68,6 +75,7 @@ function actionsMenu() {
 
   let payload;
 
+  // Dependendo do número dígitado, buscarei no server o ultimo valor do sensor escolhido e retornarei pro client.
   switch (readLine) {
     case "1":
       payload = createISOMessage({
@@ -123,10 +131,13 @@ function actionsMenu() {
   }
 }
 
+// Define o fim da conexão.
 client.on('end', function() {
   console.log('Requested an end to the TCP connection');
 });
 
+
+// Receberá as actions do server aqui, caso a action seja LOG, irá imprimir o LOG ou mensagem de sensor desligado.
 client.on('data', (data) => {
   const serializedData = data.toString();
   const parsedData: IISOMessage = JSON.parse(serializedData);
@@ -136,6 +147,7 @@ client.on('data', (data) => {
   }
 });
 
+// Ao conectar, envia seu Socket para o server, para futuras conexões do server para client.
 client.connect(options, () => {
   console.log('TCP connection established with the server.');
 
@@ -149,7 +161,10 @@ client.connect(options, () => {
 
   client.write(Buffer.from(JSON.stringify(payload)));
 
+  // Chama os parâmetros apenas uma vez.
   menu_parametros();
+
+  // Chama o menu de LOG diversas vezes.
   showMenu({ milliseconds: 500 });
 });
 
